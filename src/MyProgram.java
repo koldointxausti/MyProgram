f314f4e0bd61309cd48706a0e0247385196e065f
AnderGonzalezA <agonzalezam18dw@ikzubirimanteo.com>
2018-10-26 11:17:54 +0200

    26/10

--- src/MyProgram.java
+++ src/MyProgram.java
@@ -4,98 +4,119 @@ public class MyProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		// Save into an array all the surnames of the people in class
-		String[] surnames = {"lazkano","intxausti","artola","alberdi","lekubide","ortiz"};
-		// Take randomly a surname between them
-		String randomSurname = surnames[new Random().nextInt(surnames.length)];
-		// Print the games' description
-		System.out.println("Welcome to the famous HANGMAN game.");
-		System.out.println("You will have to guess which surname of the people in class is hidden between the voids.");
-		System.out.println("For this, you will be able to enter 3 letters that may appear in the surname, or not. After this, you will only have a chance to guess the surname and win the game.");
-		System.out.println("Good luck, start with the first letter.");
-		// Create a char array with the length of the surname taken
-		char guessed[]=new char[randomSurname.length()];
-		// Declare a variable that will hold the number of letter that the user guesses
-		int guessedCounter=0;
-		// Open a loop that will go asking to the user a possible letter in each turn
-		int flag=0;
-		while (flag<3) {
-			// Check that the user doesn't enter a number
-			if (!sc.hasNextInt()) {
-				// Create a string object with the letter's value
-				String possibleLetter=sc.next();
-				// Check that the user has entered just a character
-				if (possibleLetter.length()==1) {
-					// Open a loop that will go checking if the letter the user entered is in the surname
-					int index=0;
-					while (index<randomSurname.length()) {
-						if (randomSurname.charAt(index)==possibleLetter.charAt(0)) {
-							// If it is, open another loop which will go checking if the user had already guessed that letter
-							boolean found = false;
-							int index2=0;
-							while (index2<guessed.length && guessed[index2]!=possibleLetter.charAt(0) && !found ){
-								if (guessed[index2]==guessed[3]) {
-									// If he/she had not done so, save the letter in the array 'guessed'
-									guessed[guessedCounter] = randomSurname.charAt(index);
-									// Increment once the variable 'guessedCounter' for a future possible letter
-									guessedCounter++;
-									// Define the variable 'found' as true so as to get out of the loop
-									found = true;
+		boolean playAgain=true;
+		while (playAgain == true) {
+			// Save into an array all the surnames of the people in class
+			String[] surnames = {"lazkano","intxausti","artola","alberdi","lekubide","ortiz","gonzalez"};
+			// Take randomly a surname between them
+			String randomSurname = surnames[new Random().nextInt(surnames.length)];
+			// Print the games' description
+			System.out.println("Welcome to the famous HANGMAN game.");
+			System.out.println("You will have to guess which surname of the people in class is hidden between the voids.");
+			System.out.println("For this, you will be able to enter 3 letters that may appear in the surname, or not. After this, you will only have a chance to guess the surname and win the game.");
+			System.out.println("Good luck, start with the first letter.");
+			// Create a char array with the length of the surname taken
+			char guessed[]=new char[randomSurname.length()];
+			// Declare a variable that will hold the number of letter that the player guesses
+			int guessedCounter=0;
+			// Print the word with voids
+			for (int index3=0;index3<randomSurname.length();index3++) {
+				System.out.print("_ ");
+			}
+			// Open a loop that will go asking to the player a possible letter in each turn
+			int flag=0;
+			while (flag<3) {
+				// Check that the player doesn't enter a number
+				if (!sc.hasNextInt()) {
+					// Create a string object with the letter's value
+					String possibleLetter=sc.next();
+					// Check that the player has entered just a character
+					if (possibleLetter.length()==1) {
+						// Open a loop that will go checking if the letter the player entered is in the surname
+						for (int index=0;index<randomSurname.length();index++) {
+							if (randomSurname.charAt(index)==possibleLetter.charAt(0)) {
+								// If it is, open another loop which will go checking if the player had already guessed that letter
+								boolean found = false;
+								int index2=0;
+								while (index2<guessed.length && guessed[index2]!=possibleLetter.charAt(0) && !found ){
+									if (guessed[index2]==guessed[3]) {
+										// If he/she had not done so and the letter is in the word, save the letter in the array 'guessed'
+										guessed[guessedCounter] = randomSurname.charAt(index);
+										// Increment once the variable 'guessedCounter' for a future possible letter
+										guessedCounter++;
+										// Define the variable 'found' as true so as to get out of the loop
+										found = true;
+									}
+									// Increment the variable 'index2' so as to check the next letter of the array 'guessed'
+									index2++;
 								}
-								// Increment the the variable 'index2' so as to check the next letter
-								index2++;
 							}
 						}
-						// Increment the variable 'index' so as to check the next letter
-						index++;
+						// Increment 'flag' so as to ask for another letter
+						flag++;
 					}
-					// Increment the variable 'flag' so as to ask the next letter
-					flag++;
+					// If the player entered more than one characters, ask him/her to enter just one
+					else {
+						System.out.println("Don't cheat, please enter just a letter.");
+					}
 				}
-				// If the user entered more than one characters, ask him/her to enter just one
+				// If the player entered a number, ask him to enter a letter
 				else {
-					System.out.println("Don't cheat, please enter just a letter.");
+					System.out.println("That is not a letter, try it again.");
+					// Jump the token '/n'
+					sc.next();
 				}
-			}
-			// If the user entered a number, ask him to enter a letter
-			else {
-				System.out.println("That is not a letter, try it again.");
-				// Jump the token '/n'
-				sc.next();
-			}
-			// Printing the surname with only the letters that the user guessed
-			int index3=0;
-			while (index3<randomSurname.length()) {
-				boolean found = false;
-				int index4=0;
-				while (index4<guessed.length) {
-					if (guessed[index4]==randomSurname.charAt(index3)) {
-						System.out.print(randomSurname.charAt(index3)+ " ");
-						found = true;
+				// Print the surname with only the letters that the player guessed
+				for (int index4=0;index4<randomSurname.length();index4++) {
+					boolean found = false;
+					for (int index5=0;index5<guessed.length;index5++) {
+						if (guessed[index5]==randomSurname.charAt(index4)) {
+							System.out.print(randomSurname.charAt(index4)+ " ");
+							found = true;
+						}
 					}
-					index4++;
+					if (!found) {
+						System.out.print("_ ");
+					}
 				}
-				if (!found) {
-					System.out.print("_ ");
+				// Print to the letters that the player has left
+				if (flag == 1) {
+					System.out.println("You have two letters left.");
 				}
-				index3++;
+				else if (flag==2){
+					System.out.println("You have just a letter left.");
+				}
 			}
-			// Print to the user the letters he/she has left
-			if (flag == 1) {
-				System.out.println("You have two letters left.");
+			// Ask to the player a possible surname
+			System.out.println("It's the time, you have to guess the surname.");
+			String userRandom = sc.next();
+			if (userRandom.equals(randomSurname)) {
+				System.out.println("Congrats, you won the game.");
 			}
-			else if (flag==2){
-				System.out.println("You have just a letter left.");
+			else {
+				System.out.println("I'm sorry, you lost the game. The surname was " + randomSurname);
 			}
-		}
-		// Ask to the user a possible surname
-		System.out.println("It's the time, you have to guess the surname.");
-		String userRandom = sc.next();
-		if (userRandom.equals(randomSurname)) {
-			System.out.println("Congrats, you won the game.");
-		}
-		else {
-			System.out.println("I'm sorry, you lost the game. The surname was " + randomSurname);
+			// Ask if the player wants to play again
+			System.out.println("Would you like to play again?(y/n)");
+			boolean askAgain = true;
+			while (askAgain==true) {
+				String back = sc.next();
+				switch ( back ) {
+				   	case "y":
+				   		// Get out of the loop and play again
+				   		askAgain=false;
+				   		break;
+				   	case "n":
+				   		// Get out of the loop and terminate the program
+				   		playAgain=false;
+				   		askAgain=false;
+				   		break;
+				   	default:
+				   		// Ask for a possible value again
+				   		System.out.println("Please, select a possible value(y/n)");
+				   		break;
+			   	}
+			}
 		}
 		sc.close();
 	}
--- src/MyProgram2.java
+++ src/MyProgram2.java
@@ -3,118 +3,134 @@ public class MyProgram2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		// Print the games' description
-		System.out.println("Welcome to the famous HANGMAN game.");
-		System.out.println("First of all, someone who is not playing has to enter a word.");
-		System.out.println("Then, the player will have to guess which word is hidden between the voids.");
-		System.out.println("For this, you will be able to enter 3 letters that may appear in the word, or not. After this, you will only have a chance to guess the word hidden and win the game.");
-		System.out.println("Good luck, start with the first letter.");
-		// Open a loop that will go asking for a word to someone who is not playing
-		boolean wordFound=false;
-		while (wordFound==false) {
-			// Check that we don't enter a number
-			if (!sc.hasNextInt()) {
-				wordFound=true;
-				String word = sc.next();
-				int spaceCounter=0;
-				while (spaceCounter<20) {
-					System.out.println("");
-					spaceCounter++;
-				}
-				// Create a char array with the length of the word
-				char guessed[]=new char[word.length()];
-				// Declare a variable that will hold the number of letter that the user guesses
-				int guessedCounter=0;
-				// Printing the word with voids
-				int index3=0;
-				while (index3<word.length()) {
+		boolean playAgain=true;
+		while (playAgain == true) {
+			// Print the games' description
+			System.out.println("Welcome to the famous HANGMAN game V2.");
+			System.out.println("First of all, someone who is not playing has to enter a word.");
+			System.out.println("Then, the player will have to guess which word is hidden between the voids.");
+			System.out.println("For this, you will be able to enter 3 letters that may appear in the word, or not. After this, you will only have a chance to guess the word hidden and win the game.");
+			// Open a loop that will go asking for a word to someone who is not playing
+			boolean wordEntered=false;
+			while (wordEntered==false) {
+				System.out.println("Please, enter a word that the player doesn't know.");
+				// Check that he/she doesn't enter a number
+				if (!sc.hasNextInt()) {
+					wordEntered=true;
+					String word = sc.next();
+					// Hide the word to the player
+					for (int spaceCounter=0;spaceCounter<20;spaceCounter++) {
+						System.out.println("");
+					}
+					// Create a char array with the length of the word
+					char guessed[]=new char[word.length()];
+					// Declare a variable that will hold the number of letters that the player guesses
+					int guessedCounter=0;
+					// Print the word with voids
+					for (int index3=0;index3<word.length();index3++) {
 						System.out.print("_ ");
-					index3++;
-				}
-				// Open a loop that will go asking to the user a possible letter in each turn
-				int flag=0;
-				while (flag<3) {
-					// Check that the user doesn't enter a number
-					if (!sc.hasNextInt()) {
-						// Create a string object with the letter's value
-						String possibleLetter=sc.next();
-						// Check that the user has entered just a character
-						if (possibleLetter.length()==1) {
-							// Open a loop that will go checking if the letter the user entered is in the surname
-							int index=0;
-							while (index<word.length()) {
-								if (word.charAt(index)==possibleLetter.charAt(0)) {
-									// If it is, open another loop which will go checking if the user had already guessed that letter
+					}
+					System.out.println("Good luck, start with the first letter.");
+					// Open a loop that will go asking to the player a possible letter in each turn
+					int flag=0;
+					while (flag<3) {
+						// Check that the player doesn't enter a number
+						if (!sc.hasNextInt()) {
+							// Create a string object with the letter's value
+							String possibleLetter=sc.next();
+							// Check that the player has entered just a character
+							if (possibleLetter.length()==1) {
+								// Open a loop that will go checking if the letter the player entered is in the word
+								for (int index=0;index<word.length();index++) {
+									if (word.charAt(index)==possibleLetter.charAt(0)) {
+										// If it is, open another loop which will go checking if the player had already guessed that letter
+										boolean found = false;
+										int index2=0;
+										while (index2<guessed.length && guessed[index2]!=possibleLetter.charAt(0) && !found ){
+											if (guessed[index2]==guessed[3]) {
+												// If he/she had not done so and the letter is in the word, save the letter in the array 'guessed'
+												guessed[guessedCounter] = word.charAt(index);
+												// Increment once the variable 'guessedCounter' for a future possible letter
+												guessedCounter++;
+												// Define the variable 'found' as true so as to get out of the loop
+												found = true;
+											}
+											// Increment the variable 'index2' so as to check the next letter of the array 'guessed'
+											index2++;
+										}
+									}
+								}
+								// Print the word with only the letters that the player guessed
+								for (int index4=0;index4<word.length();index4++) {
 									boolean found = false;
-									int index2=0;
-									while (index2<guessed.length && guessed[index2]!=possibleLetter.charAt(0) && !found ){
-										if (guessed[index2]==guessed[3]) {
-											// If he/she had not done so, save the letter in the array 'guessed'
-											guessed[guessedCounter] = word.charAt(index);
-											// Increment once the variable 'guessedCounter' for a future possible letter
-											guessedCounter++;
-											// Define the variable 'found' as true so as to get out of the loop
+									for (int index5=0;index5<guessed.length;index5++) {
+										if (guessed[index5]==word.charAt(index4)) {
+											System.out.print(word.charAt(index4)+ " ");
 											found = true;
 										}
-										// Increment the the variable 'index2' so as to check the next letter
-										index2++;
+									}
+									if (!found) {
+										System.out.print("_ ");
 									}
 								}
-								// Increment the variable 'index' so as to check the next letter
-								index++;
+								// Increment the variable 'flag' so as to ask the next letter
+								flag++;
 							}
-							// Printing the word with only the letters that the user guessed
-							int index4=0;
-							while (index4<word.length()) {
-								boolean found = false;
-								int index5=0;
-								while (index5<guessed.length) {
-									if (guessed[index5]==word.charAt(index4)) {
-										System.out.print(word.charAt(index4)+ " ");
-										found = true;
-									}
-									index5++;
-								}
-								if (!found) {
-									System.out.print("_ ");
-								}
-								index4++;
+							// If the player entered more than one characters, ask him/her to enter just one
+							else {
+								System.out.println("Don't cheat, please enter just a letter.");
 							}
-							// Increment the variable 'flag' so as to ask the next letter
-							flag++;
 						}
-						// If the user entered more than one characters, ask him/her to enter just one
+						// If the player entered a number, ask him to enter a letter
 						else {
-							System.out.println("Don't cheat, please enter just a letter.");
+							System.out.println("That is not a letter, try it again.");
+							// Jump the token '/n'
+							sc.next();
+						}
+						// Print to the player the letters he/she has left
+						if (flag == 1) {
+							System.out.println("You have two letters left.");
+						}
+						else if (flag==2){
+							System.out.println("You have just a letter left.");
 						}
 					}
-					// If the user entered a number, ask him to enter a letter
+					// Ask to the player a possible word
+					System.out.println("It's the time, you have to guess the surname.");
+					String userRandom = sc.next();
+					if (userRandom.equals(word)) {
+						System.out.println("Congrats, you won the game.");
+					}
 					else {
-						System.out.println("That is not a letter, try it again.");
-						// Jump the token '/n'
-						sc.next();
+						System.out.println("I'm sorry, you lost the game. The word was " + word);
 					}
-					// Print to the user the letters he/she has left
-					if (flag == 1) {
-						System.out.println("You have two letters left.");
-					}
-					else if (flag==2){
-						System.out.println("You have just a letter left.");
+					// Ask if the player wants to play again
+					System.out.println("Would you like to play again?(y/n)");
+					boolean askAgain = true;
+					while (askAgain==true) {
+						String back = sc.next();
+						switch ( back ) {
+						   	case "y":
+						   		// Get out of the loop and play again
+						   		askAgain=false;
+						   		break;
+						   	case "n":
+						   		// Get out of the loop and terminate the program
+						   		playAgain=false;
+						   		askAgain=false;
+						   		break;
+						   	default:
+						   		// Ask for a possible value again
+						   		System.out.println("Please, select a possible value(y/n)");
+						   		break;
+					   	}
 					}
 				}
-				// Ask to the user a possible word
-				System.out.println("It's the time, you have to guess the surname.");
-				String userRandom = sc.next();
-				if (userRandom.equals(word)) {
-					System.out.println("Congrats, you won the game.");
-				}
+				// If he/she entered a number, ask him/her to enter a word
 				else {
-					System.out.println("I'm sorry, you lost the game. The word was " + word);
+					System.out.println("Enter a word please");
+					sc.next();
 				}
-			}
-			else {
-				System.out.println("Enter a word please");
-				sc.next();
 			}
 		}
 		sc.close();
